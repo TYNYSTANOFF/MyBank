5c42b340a48d59aefa3acb02ebe58b9569305748
TYNYSTANOFF <tynystanov06@gmail.com>
2025-10-27 17:31:04 +0600

    add detail screen

---
+++ app/src/main/java/com/example/mybank/ui/activity/AccountDetailsActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mybank.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mybank.R
+
+class AccountDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_account_details)
+
+    }
+}
